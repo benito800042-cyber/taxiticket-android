512709085fea8f32c80f69272fad1734d5416d95
benito800042-cyber <benito800042@gmail.com>
2026-08-27 23:17:07 +0200

    Restore compilable MainActivity

--- app/src/main/java/com/taxiticket/app/MainActivity.java
+++ app/src/main/java/com/taxiticket/app/MainActivity.java
@@ -1,1 +1,14 @@
- extends Activity
+package com.taxiticket.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+public class MainActivity extends Activity {
+ @Override public void onCreate(Bundle state) {
+  super.onCreate(state);
+  WebView web = new WebView(this);
+  web.getSettings().setJavaScriptEnabled(true);
+  web.getSettings().setDomStorageEnabled(true);
+  setContentView(web);
+  web.loadUrl("file:///android_asset/index.html");
+ }
+}
